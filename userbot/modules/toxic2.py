4fcf1660d34bd40a650b4344bf1c6f58e14b900f
rey007123 <66903937+rey007123@users.noreply.github.com>
2021-10-21 02:27:36 +0700

    Update toxic2.py

--- userbot/modules/toxic2.py
+++ userbot/modules/toxic2.py
@@ -125,350 +125,85 @@ async def typewriter(typew):
     
 # Create by myself @localheart
 
-CMD_HELP.update({
-    "toxic2":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngentot`\
-    \n↳ : Lu Coba Sendiri Aja."
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.goblok`\
-    \n↳ : Lu Coba Sendiri Aja."
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngatain`\
-    \n↳ : Lu Coba Sendiri Aja."
-})
-    await typew.edit("I")
-    
-    sleep(0.02)
-    await typew.edit("In")
-    
-    sleep(0.02)
-    await typew.edit("Ind")
-    
-    sleep(0.02)
-    await typew.edit("Inda")
-    
-    sleep(0.02)
-    await typew.edit("Indah")
-    
-    sleep(0.02)
-    await typew.edit("Indahn")
-    
-    sleep(0.02)
-    await typew.edit("Indahny")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya L")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya La")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lau")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Laut")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lauta")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan t")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan ta")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak S")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Se")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Sei")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Sein")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seind")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seinda")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah D")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Di")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Dir")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Diri")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Dirim")
-    
-    sleep(0.05)
-    await typew.edit("Indahnya Lautan tak Seindah Dirimu")
-    sleep(1.5)
-    await typew.edit("`Manisnya Gula tak Semanis Dirimu`")
-    sleep(1.5)
-    await typew.edit("Tetapi di balik Kelebihan ada Kekurangan Dirimu")
-    sleep(1.5)
-    await typew.edit("Salah Satunya Kuningnya Gigimu xixixixqqqq :v ")
-
-    
-# Create by myself @localheart
-
-CMD_HELP.update({
-    "toxic2":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngentot`\
-    \n↳ : Lu Coba Sendiri Aja."
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.goblok`\
-    \n↳ : Lu Coba Sendiri Aja."
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngatain`\
-    \n↳ : Lu Coba Sendiri Aja."
-})
-from time import sleep
-from userbot import CMD_HELP
-from userbot.events import register
-
-
-@register(outgoing=True, pattern='^.lewat(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1.2)
-    await typew.edit("**Ikan Bandeng**")
-    sleep(1.2)
-    await typew.edit("**Makan Kawat**")
-    sleep(1.2)
-    await typew.edit("**Misii Orang ganteng mau lewat 🤗**")
-# Create by myself @localheart
-
-
-@register(outgoing=True, pattern='^.ndaklevel(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1.3)
-    await typew.edit("**Buah Apel di Air Payau**")
-    sleep(1.3)
-    await typew.edit("**Ndak Level LahYauuuu**")
-# Create by myself @localheart
-
-
-@register(outgoing=True, pattern='^.hidih(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    from time import sleep
-from userbot import CMD_HELP
-from userbot.events import register
-
-
-@register(outgoing=True, pattern='^.lewat(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1.2)
-    await typew.edit("**Ikan Bandeng**")
-    sleep(1.2)
-    await typew.edit("**Makan Kawat**")
-    sleep(1.2)
-    await typew.edit("**Misii Orang ganteng mau lewat 🤗**")
-# Create by myself @localheart
-
-
-@register(outgoing=True, pattern='^.ndaklevel(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1.3)
-    await typew.edit("**Buah Apel di Air Payau**")
-    sleep(1.3)
-    await typew.edit("**Ndak Level LahYauuuu**")
-# Create by myself @localheart
-
-
-@register(outgoing=True, pattern='^.hidih(?: |$)(.*)')
+@register(outgoing=True, pattern='^.lopyu(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("I")
     sleep(0.02)
-    await typew.edit("In")
+    await typew.edit("Ik")
     sleep(0.02)
-    await typew.edit("Ind")
+    await typew.edit("Ika")
     sleep(0.02)
-    await typew.edit("Inda")
+    await typew.edit("Ikan")
     sleep(0.02)
-    await typew.edit("Indah")
+    await typew.edit("Ikan C")
     sleep(0.02)
-    await typew.edit("Indahn")
+    await typew.edit("Ikan Cu")
     sleep(0.02)
-    await typew.edit("Indahny")
+    await typew.edit("Ikan Cuc")
     sleep(0.02)
-    await typew.edit("Indahnya")
+    await typew.edit("Ikan Cucu")
     sleep(0.02)
-    await typew.edit("Indahnya L")
+    await typew.edit("Ikan Cucut")
     sleep(0.02)
-    await typew.edit("Indahnya La")
+    await typew.edit("Ikan Cucut ,")
     sleep(0.02)
-    await typew.edit("Indahnya Lau")
+    await typew.edit("Ikan Cucut ,I")
     sleep(0.02)
-    await typew.edit("Indahnya Laut")
+    await typew.edit("Ikan Cucut ,Ik")
     sleep(0.02)
-    await typew.edit("Indahnya Lauta")
+    await typew.edit("Ikan Cucut ,Ika")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan")
+    await typew.edit("Ikan Cucut ,Ikan")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan t")
+    await typew.edit("Ikan Cucut ,Ikan H")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan ta")
+    await typew.edit("Ikan Cucut ,Ikan Hi")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak")
+    await typew.edit("Ikan Cucut ,Ikan Hiu")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak S")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Se")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Sei")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r s")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Sein")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so ")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seind")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so C")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seinda")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cu")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah D")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cut")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Di")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cute")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Dir")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cute ,I")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Diri")
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cute ,I l")
     sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Dirim")
-    sleep(0.05)
-    await typew.edit("Indahnya Lautan tak Seindah Dirimu")
-    sleep(1.5)
-    await typew.edit("`Manisnya Gula tak Semanis Dirimu`")
-    sleep(1.5)
-    await typew.edit("Tetapi di balik Kelebihan ada Kekurangan Dirimu")
-    sleep(1.5)
-    await typew.edit("Salah Satunya Kuningnya Gigimu xixixixqqqq :v ")
-
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cute ,I lo")
+    sleep(0.02)
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cute ,I lov")
+    sleep(0.02)
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cute ,I love")
+    sleep(0.02)
+    await typew.edit("Ikan Cucut ,Ikan Hiu
+                      U'r so Cute ,I love u")
     
-# Create by myself @localheart
-
-CMD_HELP.update({
-    "toxic2":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngentot`\
-    \n↳ : Lu Coba Sendiri Aja."
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.goblok`\
-    \n↳ : Lu Coba Sendiri Aja."
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngatain`\
-    \n↳ : Lu Coba Sendiri Aja."
-})
-    await typew.edit("I")
-    
-    sleep(0.02)
-    await typew.edit("In")
-    
-    sleep(0.02)
-    await typew.edit("Ind")
-    
-    sleep(0.02)
-    await typew.edit("Inda")
-    
-    sleep(0.02)
-    await typew.edit("Indah")
-    
-    sleep(0.02)
-    await typew.edit("Indahn")
-    
-    sleep(0.02)
-    await typew.edit("Indahny")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya L")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya La")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lau")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Laut")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lauta")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan t")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan ta")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak S")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Se")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Sei")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Sein")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seind")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seinda")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah D")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Di")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Dir")
-    
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Diri")
-      
-    sleep(0.02)
-    await typew.edit("Indahnya Lautan tak Seindah Dirim")
-    
-    sleep(0.05)
-    await typew.edit("Indahnya Lautan tak Seindah Dirimu")
-    sleep(1.5)
-    await typew.edit("`Manisnya Gula tak Semanis Dirimu`")
-    sleep(1.5)
-    await typew.edit("Tetapi di balik Kelebihan ada Kekurangan Dirimu")
-    sleep(1.5)
-    await typew.edit("Salah Satunya Kuningnya Gigimu xixixixqqqq :v ")
-
-    
-# Create by myself @localheart
-
 CMD_HELP.update({
     "toxic2":
     "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngentot`\
