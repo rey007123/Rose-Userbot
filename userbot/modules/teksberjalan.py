7dac46a27b9d2283f07070a54f5a53308dcef25d
rey007123 <66903937+rey007123@users.noreply.github.com>
2021-10-21 01:18:05 +0700

    Update teksberjalan.py

--- userbot/modules/teksberjalan.py
+++ userbot/modules/teksberjalan.py
@@ -3,13 +3,9 @@ import sys
 import time
 
 @register(outgoing=True, pattern='^.hayyo(?: |$)(.*)')
-async def typewriter(s):
+async def mengetik(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
-#kecepatan mengetik
         time.sleep(random.random() * 0.1)
-#ubah angka 0.1 sesuai keinginan kamu
-#angka terkecil adalah yang paling cepat
-#angka terbesar adalah yang paling lambat
-typewriter('**Ikan Cucut \Ur so Cute \I love u**')
+mengetik('**Ikan Cucut \Ur so Cute \I love u**')
