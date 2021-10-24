f1e499fdce3b2da3730c24d0bb5df56fd30a6b9e
rey007123 <66903937+rey007123@users.noreply.github.com>
2021-10-24 13:27:43 +0700

    Create ocehanku.py

---
+++ userbot/modules/ocehanku.py
@@ -0,0 +1,25 @@
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.sabar(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`Sabar aja lah ya`")
+    sleep(0.7)
+    await typew.edit("`\Sabar aja lah ya`"
+                     "`\Hati kamu itu kan kaya Tango`")
+    sleep(0.7)
+    await typew.edit("`\Sabar aja lah ya`"
+                     "`\Hati kamu itu kan kaya Tango`"
+                     "`\Berapa lapis?`")
+    sleep(0.7)
+    await typew.edit("`\Sabar aja lah ya`"
+                     "`\Hati kamu itu kan kaya Tango`"
+                     "`\Berapa lapis?`"
+                     "`\Ratusan😅`")
+
+    
+
+})            
