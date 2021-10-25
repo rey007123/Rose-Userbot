eb95fa32bb4bb8d233ce99647fca1090da12c240
rey007123 <66903937+rey007123@users.noreply.github.com>
2021-10-25 12:18:30 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -91,7 +91,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             await asyncio.sleep(5)
             return await event.delete()
         else:
-            await event.edit("`Rose-Userbot Berhasil DiUpdate🛃,Restart Tunggu Sebentar`")
+            await event.edit("`ReiiUbot💤 Berhasil DiUpdate🛃,Restart Tunggu Sebentar`")
             await asyncio.sleep(15)
             await event.delete()
 
@@ -115,9 +115,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    await event.edit('**🌹Rose-Userbot🌹** `Berhasil Di Update!`')
+    await event.edit('**ReiiUbot😁** `Berhasil Di Update!`')
     await asyncio.sleep(1)
-    await event.edit('**🌹Rose-Userbot🌹** `Di Restart....`')
+    await event.edit('**ReiiUbot🥺** `Di Restart....`')
     await asyncio.sleep(1)
     await event.edit('`Mohon Menunggu Beberapa Detik.`')
     await asyncio.sleep(10)
@@ -126,7 +126,7 @@ async def update(event, repo, ups_rem, ac_br):
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID, "#BOT \n"
-            "**🌹Rose-Userbot🌹 Telah Di Perbarui.**")
+            "**REIIUBOT 😘 Telah Di Perbarui.**")
         await asyncio.sleep(100)
         await event.delete()
 
@@ -188,13 +188,13 @@ async def upstream(event):
 
     if changelog == '' and force_update is False:
         await event.edit(
-            f'\n🌹Rose-Userbot🌹 Sudah Versi Terbaru || Tunggu Update Terbaru\n')
+            f'\nReiiUbot💤 Sudah Versi Terbaru || Tunggu Update Terbaru\n')
         await asyncio.sleep(15)
         await event.delete()
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**Pembaruan Untuk 🌹Rose-Userbot🌹 :\n\n⚒️ Pembaruan Data :**\n`{changelog}`'
+        changelog_str = f'**Pembaruan Untuk Reiiubot💤 :\n\n⚒️ Pembaruan Data :**\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
             file = open("output.txt", "w+")
@@ -214,12 +214,12 @@ async def upstream(event):
         await event.edit(
             '`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`')
     else:
-        await event.edit('` Proses Update 🌹Rose-Userbot🌹, Loading....1%`')
-        await event.edit('` Proses Update 🌹Rose-Userbot🌹 Loading....20%`')
-        await event.edit('` Proses Update 🌹Rose-Userbot🌹, Loading....35%`')
-        await event.edit('` Proses Update 🌹Rose-Userbot🌹, Loading....77%`')
-        await event.edit('` Proses Update 🌹Rose-Userbot🌹, Updating...90%`')
-        await event.edit('` Proses Update 🌹Rose-Userbot🌹, Mohon Tunggu Sebentar....100%`')
+        await event.edit('` Proses Update ReiiUbot💤, Loading....1%`')
+        await event.edit('` Proses Update ReiiUbot💤, Loading....20%`')
+        await event.edit('` Proses Update ReiiUbot💤, Loading....35%`')
+        await event.edit('` Proses Update ReiiUbot💤, Loading....77%`')
+        await event.edit('` Proses Update ReiiUbot💤, Updating...90%`')
+        await event.edit('` Proses Update ReiiUbot💤, Mohon Sabar ya Reii...😘 ....100%`')
 
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
